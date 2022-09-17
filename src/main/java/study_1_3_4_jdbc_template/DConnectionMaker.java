871b67f72ccf7b2ab61ca58752d861690658b7b9
leenuu <kangjoon10@gmail.com>
2022-09-17 15:18:54 +0900

    test

--- src/main/java/study_1_3_4_jdbc_template/DConnectionMaker.java
+++ src/main/java/study_1_3_4_jdbc_template/DConnectionMaker.java
@@ -8,7 +8,7 @@ public class DConnectionMaker implements ConnectionMaker {
     public Connection makeConnection() throws ClassNotFoundException, SQLException {
 		Class.forName("com.mysql.cj.jdbc.Driver");
         Connection c = DriverManager.getConnection("jdbc:mysql://15.164.225.156/test", "root", "Kangjoon12!");
-//        System.out.println("connection complete!");
+        System.out.println("connection complete!");
 
         return c;
     }
--- src/main/java/study_1_3_4_jdbc_template/UserDao.java
+++ src/main/java/study_1_3_4_jdbc_template/UserDao.java
@@ -1,17 +1,8 @@
 package study_1_3_4_jdbc_template;
 
-<<<<<<< HEAD
-import org.springframework.dao.DataAccessException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
-import org.springframework.jdbc.core.ResultSetExtractor;
-import org.springframework.jdbc.core.RowMapper;
-=======
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
->>>>>>> 12f2624eb0d2b4791e1d16c85a44d28abc0d74b0
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -24,7 +15,7 @@ public class UserDao {
     private DataSource dataSource;
 //    private JdbcContext jdbcContext;
 
-//    public void setJdbcContext(JdbcContext jdbcContext) {
+    //    public void setJdbcContext(JdbcContext jdbcContext) {
 //        this.jdbcContext = jdbcContext;
 //    }
     public void setDataSource(DataSource dataSource) {
@@ -36,40 +27,16 @@ public class UserDao {
     }
 
     public void add(final User user) throws SQLException {
-<<<<<<< HEAD
-=======
 //       this.jdbcContext.exequteSql("insert into users(id, name, password) values(?,?,?)", user.getid(), user.getname(), user.getpassword());
->>>>>>> 12f2624eb0d2b4791e1d16c85a44d28abc0d74b0
-        this.jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)", user.getid(), user.getname(), user.getpassword());
+//        this.jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)", user.getid(), user.getname(), user.getpassword());
     }
 
     public User get(String id) throws SQLException {
-<<<<<<< HEAD
-        return this.jdbcTemplate.queryForObject("select * from users where id = ?",
-                new Object[]{id},
-                new RowMapper<User>() {
-                    public User mapRow(ResultSet resultSet, int i) throws SQLException {
-                        User user =new User();
-                        user.setid(resultSet.getString("id"));
-                        user.setname(resultSet.getString("name"));
-                        user.setpassword(resultSet.getString("password"));
-
-                        return user;
-                    }
-                });
-    }
-    public int getCount() throws SQLException{
-        return this.jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
-//        return 0;
-    }
-
-    public void reset() throws SQLException {
-=======
         Connection c = dataSource.getConnection();
         PreparedStatement ps = c.prepareStatement(
-            "select * from users where id = ?");
-        
-    	ps.setString(1, id);
+                "select * from users where id = ?");
+
+        ps.setString(1, id);
 
         ResultSet rs = ps.executeQuery();
 
@@ -81,7 +48,7 @@ public class UserDao {
             user.setpassword((rs.getString("password")));
         }
 
-		rs.close();
+        rs.close();
         ps.close();
         c.close();
 //		System.out.println("get user inform complete!");
@@ -104,7 +71,6 @@ public class UserDao {
     }
     public void reset() throws SQLException {
 //        this.jdbcContext.exequteSql("TRUNCATE users");
->>>>>>> 12f2624eb0d2b4791e1d16c85a44d28abc0d74b0
         this.jdbcTemplate.update("TRUNCATE users");
     }
 
